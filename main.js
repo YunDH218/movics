ddad50a27c07e433f14bfed7d4bf3281743129c6
unknown <68414997+YunDH218@users.noreply.github.com>
2022-03-11 22:34:53 +0900

    review-top

---
+++ main.js
@@ -0,0 +1,36 @@
+const searchBtnEl = document.querySelector('.submenu .btn_search')
+const menuBtnEl = document.querySelector('.submenu .btn_menu')
+const menuEl = document.querySelector('header .main-menu')
+const menuBGEl = document.querySelector('header .menu-background')
+
+menuBtnEl.addEventListener('click', function () {
+  gsap.to(menuEl, .6, {
+    display: 'block',
+    right: 0
+  })
+  gsap.to(menuBGEl, .6, {
+    display: 'block',
+    opacity: 1
+  })
+  menuBtnEl.setAttribute('style', 'color: #c00')
+  searchBtnEl.setAttribute('style', 'color: #eee')
+})
+menuBGEl.addEventListener('click', function() {
+  gsap.to(menuEl, .6, {
+    display: 'none',
+    right: '-25em',
+  })
+  gsap.to(menuBGEl, .6, {
+    display: 'none',
+    opacity: 0,
+  })
+  menuBtnEl.setAttribute('style', 'color: #eee')
+  searchBtnEl.setAttribute('style', 'color: #c00')
+})
+
+new Swiper('.review-top .swiper-container', {
+  slidesPerView: 6,
+  // spaceBetween: 1,
+  // centeredSlides: true,
+  //loop: true,
+});
