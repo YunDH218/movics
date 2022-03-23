d026537d9eb925f3b69157f85a9bf331d81765ad
YunDH218 <3500ml@naver.com>
2022-03-23 20:14:26 +0900

    mypage-complete

--- js/mypage.js
+++ js/mypage.js
@@ -1,6 +1,16 @@
 btnEls = document.querySelectorAll('.container .btn');
 contentEls = document.querySelectorAll('.container .content');
 
-btnEls.forEach( function (btnEl, index) {
-  btnEl.classList.add(`active`)
+btnEls.forEach((btnEl, index) => {
+  btnEl.addEventListener('click', () => {
+    for (let i = 0; i < btnEls.length; i++) {
+      btnEls[i].classList.remove('active')
+      contentEls[i].classList.remove('active')
+    }
+    btnEl.classList.add('active')
+    if (btnEl.classList.contains('active')) {
+      contentEls[index].classList.add('active')
+    }
+  })
 });
+
